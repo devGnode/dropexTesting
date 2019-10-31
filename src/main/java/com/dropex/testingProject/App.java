cc97101250c8e35b7932bcea4bf76af180ce136b
madevincre <marc_antoine_devincre@carrefour.com>
2019-10-31 12:34:13 +0100

    init project

---
+++ src/main/java/com/dropex/testingProject/App.java
@@ -0,0 +1,13 @@
+package com.dropex.testingProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
